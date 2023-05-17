8622c79a7a90c9e89b88491811d86c69f5779da8
Balint Orosz <orosz.balint.97@gmail.com>
2023-05-17 22:31:03 +0200

    fix: playwright on CI

--- apps/app-e2e/src/example.spec.ts
+++ apps/app-e2e/src/example.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@playwright/test";
 
 test("has hello traveler title", async ({ page }) => {
-  await page.goto("http://localhost:5173");
+  await page.goto(process.env.E2E_URL ?? "http://localhost:5173");
 
   expect(page.getByText("Hello Traveler!")).toBeTruthy();
 });
