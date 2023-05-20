f6d587ea36426742fcfa1858ddf31d2486a967e9
Balint Orosz <blntrsz@pop-os.localdomain>
2023-05-20 20:24:34 +0200

    feat: add new step to main pipeline

--- apps/app-tools/src/pipeline/abstract-pipeline.ts
+++ apps/app-tools/src/pipeline/abstract-pipeline.ts
@@ -1,6 +1,50 @@
+import * as cdk from "aws-cdk-lib";
+import { BuildSpec, LinuxBuildImage, Project } from "aws-cdk-lib/aws-codebuild";
 import { Artifact, Pipeline } from "aws-cdk-lib/aws-codepipeline";
-import { CodeStarConnectionsSourceAction } from "aws-cdk-lib/aws-codepipeline-actions";
+import {
+  CodeBuildAction,
+  CodeStarConnectionsSourceAction,
+} from "aws-cdk-lib/aws-codepipeline-actions";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
+
+class AbstractCodeBuildProject extends Construct {
+  codebuildAction: cdk.aws_codepipeline_actions.CodeBuildAction;
+  constructor(
+    scope: Construct,
+    id: string,
+    source: cdk.aws_codepipeline.Artifact
+  ) {
+    super(scope, id);
+    const project = new Project(this, "project", {
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_7_0,
+      },
+      buildSpec: BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          build: {
+            commands: [""],
+          },
+        },
+      }),
+    });
+
+    project.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ["*"],
+        resources: ["*"],
+      })
+    );
+
+    this.codebuildAction = new CodeBuildAction({
+      actionName: id,
+      input: source,
+      project: project,
+    });
+  }
+}
 
 export class AbstractPipeline extends Construct {
   pipeline: Pipeline;
@@ -26,6 +70,13 @@ export class AbstractPipeline extends Construct {
     this.pipeline.addStage({
       stageName: "Source",
       actions: [sourceAction],
+    });
+
+    this.pipeline.addStage({
+      stageName: "CheckAffected",
+      actions: [
+        new AbstractCodeBuildProject(this, "asd", this.source).codebuildAction,
+      ],
     });
   }
 }
